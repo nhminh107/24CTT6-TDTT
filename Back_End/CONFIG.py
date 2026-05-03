f79e519f372ef676c00df7707811a4073da52c53
nhminh107 <24120381@student.hcmus.edu.vn>
2026-05-03 22:52:38 +0700

    Thêm database, chỉnh sửa config.

--- Back_End/CONFIG.py
+++ Back_End/CONFIG.py
@@ -1,3 +1,4 @@
+import os 
 user_lat = 0.0
 user_lng = 0.0
 
@@ -6,3 +7,5 @@ distance_weight = 0.5
 price_weight = 0.3
 another_weight = 0.2 # Khởi tạo tạm, tạm thời chưa dùng
 
+# Path 
+DB_PATH = os.path.join(os.getcwd(), 'Back_End', 'Database')
